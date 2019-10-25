63b4e8bb88141e696ac1c204f3f26206114d3789
Sergey Morozov <karandash.zapiskin@gmail.com>
2019-10-25 09:26:15 +0400

    Поиск индекса в диапазоне.[#191278]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -22,4 +22,14 @@ public class FindLoop {
         }
         return rst;
     }
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i <= finish; i++) {
+            if (el == data[i]) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,6 +5,29 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNotFind() {
+        int[] input = new int[] {5, 2, 10, 6, 4};
+        int value = 1;
+        int start = 1;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
     @Test
     public void whenArrayHas5Then0() {
         FindLoop findLoop = new FindLoop();
